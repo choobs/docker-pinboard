00a652467262eea899066c2a418ca869c2ad8e5f
Erik DeLamarter <erik.delamarter@choobs.com>
2016-04-08 12:30:10 +0200

    Fixed router.php to handle url rewrite correctly

--- router.php
+++ router.php
@@ -1,5 +1,12 @@
-<?php 
+<?php
 
 $_SERVER['SERVER_NAME'] = 'localhost';
 
-return false;
+//http://silex.sensiolabs.org/doc/web_servers.html#php-5-4
+
+$filename = $_SERVER["DOCUMENT_ROOT"] . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+require $_SERVER["DOCUMENT_ROOT"] . '/index.php';
